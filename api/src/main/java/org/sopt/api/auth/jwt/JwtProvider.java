ec087558d28ac2271b29371f6fa91b07511d4cb9
sunwoong <sunwoong1122@gmail.com>
2023-11-30 23:42:22 +0900

    [feat] #7 jwt provider 클래스 구현

---
+++ api/src/main/java/org/sopt/api/auth/jwt/JwtProvider.java
@@ -0,0 +1,23 @@
+package org.sopt.api.auth.jwt;
+
+import io.jsonwebtoken.JwtParser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class JwtProvider {
+    private final JwtGenerator jwtGenerator;
+
+    public Token issueToken(Long memberId) {
+        return Token.of(jwtGenerator.generateToken(memberId, true),
+                jwtGenerator.generateToken(memberId, false));
+    }
+
+    public Long getSubject(String token) {
+        JwtParser jwtParser = jwtGenerator.getJwtParser();
+        return Long.valueOf(jwtParser.parseClaimsJws(token)
+                .getBody()
+                .getSubject());
+    }
+}
