2113ad1f6921a8593eacabd3d993b96aaafd0a6c
sunwoong <sunwoong1122@gmail.com>
2023-11-30 23:43:30 +0900

    [feat] #7 jwt 인증 필터 클래스 구현

---
+++ api/src/main/java/org/sopt/api/auth/JwtAuthenticationFilter.java
@@ -0,0 +1,58 @@
+package org.sopt.api.auth;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.sopt.api.auth.jwt.JwtProvider;
+import org.sopt.api.auth.jwt.JwtValidator;
+import org.sopt.common.error.ErrorStatus;
+import org.sopt.common.error.UnauthorizedException;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+import static org.sopt.api.auth.UserAuthentication.createDefaultUserAuthentication;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private static final String AUTHORIZATION = "Authorization";
+    private static final String BEARER = "Bearer ";
+    private final JwtValidator jwtValidator;
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String accessToken = getAccessToken(request);
+        jwtValidator.validateAccessToken(accessToken);
+        setAuthentication(request, jwtProvider.getSubject(accessToken));
+        filterChain.doFilter(request, response);
+    }
+
+    private String getAccessToken(HttpServletRequest request) {
+        String accessToken = request.getHeader(AUTHORIZATION);
+        if (StringUtils.hasText(accessToken) && accessToken.startsWith(BEARER)) {
+            return accessToken.substring(BEARER.length());
+        }
+        throw new UnauthorizedException(ErrorStatus.INVALID_ACCESS_TOKEN);
+    }
+
+    private void setAuthentication(HttpServletRequest request, Long memberId) {
+        UserAuthentication authentication = createDefaultUserAuthentication(memberId);
+        createWebAuthenticationDetailsAndSet(request, authentication);
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(authentication);
+    }
+
+    private void createWebAuthenticationDetailsAndSet(HttpServletRequest request, UserAuthentication authentication) {
+        WebAuthenticationDetailsSource webAuthenticationDetailsSource = new WebAuthenticationDetailsSource();
+        WebAuthenticationDetails webAuthenticationDetails = webAuthenticationDetailsSource.buildDetails(request);
+        authentication.setDetails(webAuthenticationDetails);
+    }
+}
